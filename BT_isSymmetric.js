a462f34e6dc0328a852c407d87c4fb3dc30dfd02
pablomdd <pablo.mdd.007@gmail.com>
2021-03-04 18:28:36 -0600

    add problem is symmetric

---
+++ BT_isSymmetric.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+    if (root === null) return true;
+    if (!root.left && !root.right) return true;
+
+    const queue = [];
+
+    queue.push(root.left);
+    queue.push(root.right);
+
+    while (queue.length > 0) {
+        const nodeLeft = queue.shift();
+        const nodeRight = queue.shift();
+
+        if (nodeLeft === null && nodeRight === null) continue;
+        if (nodeLeft === null || nodeRight === null) return false;
+        if (nodeLeft.val !== nodeRight.val) return false;
+
+        queue.push(nodeLeft.left);
+        queue.push(nodeRight.right);
+        queue.push(nodeLeft.right);
+        queue.push(nodeRight.left);
+    }
+    return true;
+};
