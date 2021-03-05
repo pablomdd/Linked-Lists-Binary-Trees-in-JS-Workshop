1929ca9cd7452810b434bd15799e1c88022f7cef
pablomdd <pablo.mdd.007@gmail.com>
2021-03-04 18:22:42 -0600

    add kth last element

---
+++ LL_kthLastElement.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+    let dummyHead = new ListNode();
+    dummyHead.next = head;
+
+    let fast = dummyHead;
+    let slow = dummyHead;
+
+    for (let i = 1; i <= n + 1; i++) {
+        fast = fast.next;
+    }
+
+    while (fast) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    slow.next = slow.next.next;
+
+    return dummyHead.next;
+};
